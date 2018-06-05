6fb20ded61e3985a99b4cfd20fd211cda456c37b
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-06-04 20:17:22 -0500

    Github Authentication now working

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -6,9 +6,12 @@ const { expect } = require('chai');
 const urlBase = 'https://api.github.com';
 const githubUserName = 'afaguilarr';
 const repository = 'workshop-api-testing-js';
+// Sin la siguiente lista el travis CI de GitHub no deja que pase los test
+process.env.ACCESS_TOKEN = '30a15301c6ac516bf67051dd314485b04bf946dd';
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
+    // console.log(process.env.ACCESS_TOKEN);
     it('Via OAuth2 Tokens by Header', () =>
       agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
